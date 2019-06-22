a84acef2605559d590436ee9989b847be83f6d6c
Grzegorz Zych <grzegorzzych95@gmail.com>
2019-06-23 00:59:45 +0200

    img change algorithm done,
    Signed-off-by: Grzegorz Zych <grzegorzzych95@gmail.com>

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const img2 = document.querySelector('div.images :nth-child(2)')
 const img3 = document.querySelector('div.images :nth-child(3)')
 const btnminus = document.querySelector('nav :nth-child(1)')
 const btnplus = document.querySelector('nav :nth-child(2) i')
+const img = document.querySelectorAll('img')
+const imgs = Array.from(img)
 let i = 0
 
 btnplus.addEventListener('click', plusVal)
@@ -19,20 +21,40 @@ function render() {
     })
 }
 
-
 function show(resp) {
-  img1.src = resp[i].download_url
-  img2.src = resp[i + 1].download_url
-  img3.src = resp[i + 2].download_url
+  let counter = 0
+  const img1New = document.createElement('img')
+  const img2New = document.createElement('img')
+  const img3New = document.createElement('img')
+  img1New.src = resp[i].download_url
+  img2New.src = resp[i + 1].download_url
+  img3New.src = resp[i + 2].download_url;
+  [img1New, img2New, img3New].map(img => img.addEventListener('load', function () {
+    counter++
+    if (counter === 3) {
+      imgs.forEach(function (img) {
+        img.classList.remove('anim')
+      })
 
+      return setTimeout(() => {
+
+        img1.src = resp[i].download_url
+        img2.src = resp[i + 1].download_url
+        img3.src = resp[i + 2].download_url
+        imgs.forEach(function (img) {
+          img.classList.add('anim')
+        })
+      }, 100)
+    }
+  }))
 }
 
 function plusVal() {
   i += 3
   if (i > 27) {
     i = 27
+    return
   }
-  // console.log(i)
   render()
 
 }
@@ -41,8 +63,8 @@ function minusVal() {
   i -= 3
   if (i < 0) {
     i = 0
+    return
   }
-  // console.log(i)
   render()
 
 }
