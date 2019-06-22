ee2c5f66ff222e803b0811bb9522d8740aab915e
Grzegorz Zych <grzegorzzych95@gmail.com>
2019-06-22 14:16:17 +0200

    improved nav, improved sizing of img
    Signed-off-by: Grzegorz Zych <grzegorzzych95@gmail.com>

--- script.js
+++ script.js
@@ -15,11 +15,6 @@ function render() {
   fetch("https://picsum.photos/v2/list")
     .then(resp => resp.json())
     .then(resp => {
-      // Show()
-      // console.log(resp)
-      // console.log(resp[1])
-      // console.log(resp[1].url)
-      // img1.src = resp[1].url
       show(resp)
     })
 }
@@ -29,28 +24,25 @@ function show(resp) {
   img1.src = resp[i].download_url
   img2.src = resp[i + 1].download_url
   img3.src = resp[i + 2].download_url
-  // console.log(resp[1].url)
-  // console.log(resp[1].url)
-  // console.log(img2)
-  // console.log(img3)
+
 }
 
 function plusVal() {
   i += 3
-  console.log(i)
+  if (i > 27) {
+    i = 27
+  }
+  // console.log(i)
   render()
 
 }
 
 function minusVal() {
   i -= 3
-  console.log(i)
+  if (i < 0) {
+    i = 0
+  }
+  // console.log(i)
   render()
 
 }
-
-// function minusval() {
-//   i -= 3
-//   console.log(i)
-
-// }
